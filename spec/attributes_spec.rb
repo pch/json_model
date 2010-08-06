43e2987c39c1ca82d94b85d5a4d403936368b5ad
Grzegorz Kazulak <grzegorz.kazulak@gmail.com>
2010-08-06 22:39:37 +0800

    missing file

---
+++ spec/attributes_spec.rb
@@ -0,0 +1,44 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe "Attributes" do
+  it "should load an array" do
+    Array.json_load([1,2,3, "four"]).should eql([1,2,3,"four"])
+  end
+  
+  it "should dump an array" do
+    Array.json_dump([1,2,3, "four"]).should eql([1,2,3,"four"])
+  end
+  
+  it "should load an integer" do
+    Integer.json_load(1).should eql(1)
+  end
+  
+  it "should dump an integer" do
+    Integer.json_dump(1).should eql(1)
+  end
+  
+  it "should load a string" do
+    String.json_load("John").should eql("John")
+  end
+  
+  it "should dump a stringr" do
+    String.json_dump("John").should eql("John")
+  end
+  
+  it "should create new object with a string attribute" do
+    person = Person.new(:name => "Joe")
+    person.name.should eql("Joe")
+  end
+  
+  it "should create new object with a integer attribute" do
+    person = Person.new(:id => 1)
+    person.id.should eql(1)
+  end
+  
+  it "should raise an error when there is no matching attribute defined" do
+    lambda {
+      person = Person.new(:asd => 1)
+    }.should raise_error(StandardError)
+  end
+  
+end
