8e62b9b6749863c6cc2852b23591411828d23a2d
Grzegorz Kazulak <grzegorz.kazulak@gmail.com>
2010-08-06 22:35:39 +0800

    first set of tests

--- spec/fixtures.rb
+++ spec/fixtures.rb
@@ -1,3 +1,18 @@
 class Person
   include JsonModel
+  
+  attribute :name, String
+  attribute :id, Integer
+  
+  has_many :cars
+  
+end
+
+
+class Car
+  include JsonModel
+  
+  attribute :make, String
+  attribute :model, String
+  attribute :bough_on, TimeStamp
 end
