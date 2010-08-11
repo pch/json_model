19699ac2ee494be7cff64cc3b98bf4f70cf66050
Piotr Chmolowski <piotr@distortionlabs.pl>
2010-08-11 15:46:50 +0200

    Switched load widh dump (still not sure about that)

--- lib/json_model/data_types/bitfield.rb
+++ lib/json_model/data_types/bitfield.rb
@@ -1,10 +1,6 @@
 class Bitfield
   
-  def self.json_load(value)
-    value
-  end
-  
-  def self.json_dump(data)
+  def self.json_load(data)
     return data unless data.is_a?(Hash)
     
     result = 0
@@ -13,5 +9,9 @@ class Bitfield
       result ^= value if value > 0
     end
     result
+  end
+  
+  def self.json_dump(value)
+    value
   end
 end
