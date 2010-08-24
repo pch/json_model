322d2fecb1a54ae7df4c24e7456b5ac9cf721ff8
pch <piotr@chmolowski.pl>
2010-08-24 13:14:49 +0200

    Change date format

--- lib/json_model/data_types/timestamp.rb
+++ lib/json_model/data_types/timestamp.rb
@@ -1,7 +1,7 @@
 class TimeStamp
   
   def self.json_load(value)
-    if value.to_s.match(/\d{2}\/\d{2}\/\d{4}/) # TODO
+    if value.to_s.match(/\d{4}-\d{2}-\d{2}/) # TODO
       value = DateTime.parse(value)
     end    
     value.to_i
