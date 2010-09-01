2625fa26586b4df8e4a83b30330bb0db866d956a
pch <piotr@chmolowski.pl>
2010-09-01 16:21:07 +0200

    Fixed timestamp spec

--- spec/timestamp_spec.rb
+++ spec/timestamp_spec.rb
@@ -3,6 +3,6 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "TimeStamp" do
   
   it "should parse date to Integer" do
-    TimeStamp.json_load("12/12/2010").should == 1292112000
+    TimeStamp.json_load("2010-12-12").should == 1292112000
   end
 end
