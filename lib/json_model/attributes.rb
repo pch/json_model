1681eca14e11d3e7c81dcfe8022a855cd6d12dc7
pch <piotr@chmolowski.pl>
2010-11-10 12:48:25 +0100

    Exclude valid association keys (has_many) from the exception

--- lib/json_model/attributes.rb
+++ lib/json_model/attributes.rb
@@ -4,7 +4,7 @@ module JsonModel
       super(attrs)
       load_attributes(attrs)
     end
-    
+
     # Converts the current object to a hash with attribute names as keys
     # and the values of the attributes as values.
     #
@@ -18,25 +18,25 @@ module JsonModel
       end
       attrs
     end
-    
+
     # Updates existing attributes
     #
     def update_attributes(attrs)
       load_attributes(attrs)
     end
-    
+
     private
-      
+
       # Loads attributes from the attrs hash.
       #
       def load_attributes(attrs)
         attrs = symbolize_keys(attrs)
-        
+
         self.class.attributes.each do |attr_name, info|
           next unless attrs.include?(attr_name)
-          
+
           current_value = send(attr_name)
-          
+
           if !current_value.nil? && current_value.respond_to?(:update_attributes)
             current_value.update_attributes(attrs[attr_name])
           else
@@ -46,24 +46,27 @@ module JsonModel
 
           attrs.delete(attr_name)
         end
-        raise StandardError, "undefined attributes: #{attrs.inspect} for #{self.class}" if (attrs.keys - self.class.associations.keys).size > 0
+
+        if (attrs.keys - self.class.associations.keys).size > 0
+          raise StandardError, "undefined attributes: #{(attrs.keys - self.class.associations.keys).inspect} for #{self.class}"
+        end
       end
-    
+
     module ClassMethods
-        
+
       def attribute(name, klass)
         attributes.store(name, {:class => klass})
         self.send(:attr_accessor, name)
       end
-      
+
       def attributes
         @attributes ||= {}
       end
-      
+
       def json_dump(value)
         value.as_json
       end
-      
+
       def json_load(value)
         new(value)
       end
--- spec/associations_spec.rb
+++ spec/associations_spec.rb
@@ -1,20 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Associations" do
-  
+
   it "should create new object with association and assigned attrs" do
     person = Person.new :cars => [{'make' => 2010}]
     person.cars.size.should eql(1)
     person.cars[0].make.should eql('2010')
   end
-  
+
   it "should allow for association names different than the class name" do
     person = Person.new(:red_cars => [{:make => 'Subaru'}])
     person.red_cars.size.should == 1
     person.red_cars[0].should be_an_instance_of(Car)
     person.red_cars[0].make.should == "Subaru"
   end
-  
+
   it "should load associations list when it's provided as a Hash" do
     person = Person.new('cars' => {'0' => {'make' => 'Audi', 'model' => 'A3'}})
     person.cars.size.should     == 1
@@ -33,6 +33,4 @@ describe "Associations" do
       person = Person.new :cars => [{'makes' => 1}]
     }.should raise_error(StandardError)
   end
-  
-  #it "should update associations"
 end
